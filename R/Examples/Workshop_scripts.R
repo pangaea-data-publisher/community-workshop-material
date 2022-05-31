7784d015fd8d08e0d9aa389ea4387a3ab8acac47
Daniela Ransby <dpittaue@fu-00243.dmawi.de>
2022-05-31 10:26:53 +0200

    + download many (non-binary) + save results as files

--- R/Examples/Workshop_scripts.R
+++ R/Examples/Workshop_scripts.R
@@ -1,8 +1,9 @@
-install.packages('pangaear')
-install.packages('dplyr')
+# install.packages('pangaear')
+# install.packages('dplyr')
 library(pangaear) #see package details at https://github.com/ropensci/pangaear
 library(dplyr)
 #library(ggplot2)
+
 
 #=============== 1. SEARCH (by PROJECT) =================
 
@@ -19,36 +20,45 @@ PAGES <- rbind(PAGES, pg_search("project:label:PAGES_C-PEAT", count = 500, offse
 Joey_core12 <- pg_data(doi="10.1594/PANGAEA.890405")
 Joey_core12 <- Joey_core12[[1]]$data
 
-#=============== FILTER SEARCH RESULTS =================
+#create a folder for download
+getwd()
+dir.create(path="R/Files")
+folderpath <- "R/Files/"
+
+#download table as txt file
+write.table(Joey_core12, file=paste(folderpath,"Joey_core12.txt", sep=""), row.names = FALSE, quote = FALSE, sep = "\t", na = "")
+
+#=============== 3. FILTER SEARCH RESULTS =================
 # restrict the search by geographical coordinates with an attribute bbox=c(minlon, minlat, maxlon, maxlat)
 # datasets in northern Sweden
 PAGES_Sweden <- pg_search("project:label:PAGES_C-PEAT", count = 1000, bbox=c(17.7, 67.7, 21, 69))
 #filter only datasets with "Geochemistry" in title (column citation)
 PAGES_Sweden <- filter(PAGES_Sweden, grepl("Geochemistry", citation))
 
-#=============== GET MULTIPLE DATA =================
+#=============== 4. GET MULTIPLE DATA =================
 # combine data into a single data frame
 PAGES_Sweden_data <- data.frame()
+
 for (i in 1:nrow(PAGES_Sweden)) {
   geochem <- pg_data(doi=PAGES_Sweden[i,2])
   geochem <- geochem[[1]]$data
-  geochem$DOI <- PAGES_Sweden[i,2]
-  geochem$citation <- PAGES_Sweden[i,5]
+  geochem$DOI <- as.character(PAGES_Sweden[i,2])
+  geochem$citation <- as.character(PAGES_Sweden[i,5])
   PAGES_Sweden_data <- bind_rows(PAGES_Sweden_data, geochem)
 }
 
-#=============== DOWNLOAD DATA FILES VIA PANGAEAR CACHE ===================
+#download table as txt file
+write.table(PAGES_Sweden_data, file=paste(folderpath,"Sweden_geochem.txt", sep=""), row.names = FALSE, quote = FALSE, sep = "\t", na = "")
+
+
+#=============== 5. DOWNLOAD DATA FILES VIA PANGAEAR CACHE ===================
 #check and set cache path of pangaear
 pg_cache$cache_path_get()
-pg_cache$cache_path_set(full_path = "~/R/Examples")
+pg_cache$cache_path_set(full_path = "~/R/Files")
 #now data files are downloaded into that folder when executing pg_data()
 #does not work for multiple binary files
 
-#=============== DOWNLOAD MANY BINARY FILES ==================
-
-#create a folder for download
-dir.create(path="Files")
-folderpath <- "Files/"
+#=============== 6. DOWNLOAD MANY BINARY FILES ==================
 
 # download the images from a single dataset https://doi.pangaea.de/10.1594/PANGAEA.919398
 table <- pg_data(doi="10.1594/PANGAEA.919398")
@@ -64,4 +74,14 @@ for (i in (1:nrow(table))){
   if (grepl("fauna", table$'Content'[i]) == TRUE ) {
     download.file(paste(prefix, table$IMAGE[i], sep=""), destfile = paste(folderpath, table$'IMAGE'[i], sep=""))  
   }
+}
+
+#=============== 7. DOWNLOAD MANY FILES ==================
+#this concerns datasets published before 2020 (in .tab file the full path is given, not just the file name)
+
+list_of_files<-pg_data(doi="10.1594/PANGAEA.910179")
+list_of_files<-list_of_files[[1]]$data
+
+for (i in (1:nrow(list_of_files))){
+  download.file(as.character(list_of_files$'URL file'[i]), destfile = paste0(folderpath, list_of_files$'File name'[i]))
 }
