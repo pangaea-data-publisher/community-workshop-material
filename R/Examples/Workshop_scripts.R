f8106471eae1f50c8ce8260a2b3eb0d94a1e3aa6
Danapit <38073142+Danapit@users.noreply.github.com>
2022-06-01 16:40:35 +0200

    commentary, cleaning

--- R/Examples/Workshop_scripts.R
+++ R/Examples/Workshop_scripts.R
@@ -1,22 +1,22 @@
 # install.packages('pangaear')
 # install.packages('dplyr')
 library(pangaear) # see package details at https://github.com/ropensci/pangaear
-library(dplyr)
-# library(ggplot2)
-
+library(dplyr) # set of tools for data manipulation, see details at https://dplyr.tidyverse.org/
 
 #=============== 1. SEARCH (by PROJECT) =================
 # Documentation of PANGAEA search: https://wiki.pangaea.de/wiki/PANGAEA_search
 # Website: https://www.pangaea.de/?q=project:label:PAGES_C-PEAT
 # search with pg_search: maximum = 500 records (set with count, continue with offset)
-PAGES <- pg_search("project:label:PAGES_C-PEAT", count = 100)
+PAGES <- pg_search("project:label:PAGES_C-PEAT")
 
 # PAGES <- pg_search("project:label:PAGES_C-PEAT", count = 500, offset = 500)
-# like this one can download all 875 dataset citations
+# like this one can download all 875 dataset citations:
+# rbind function: joins 2 or more dataframes
 PAGES <- rbind(PAGES, pg_search("project:label:PAGES_C-PEAT", count = 500, offset = 500))
 
 #=============== 2. GET DATA =================
-# download single dataset - first list, then -> data frame
+# download single dataset (rendomly selected from the search result above)
+# pg_data returns list, data table -> data frame
 Joey_core12 <- pg_data(doi="10.1594/PANGAEA.890405")
 Joey_core12 <- Joey_core12[[1]]$data
 
@@ -25,30 +25,39 @@ getwd()
 dir.create(path="R/Files")
 folderpath <- "R/Files/"
 
-# download table as txt file
-write.table(Joey_core12, file=paste(folderpath,"Joey_core12.txt", sep=""), row.names = FALSE, quote = FALSE, sep = "\t", na = "")
+# write table as txt file
+# paste function: concatenate vectors by converting them into character (list of vectors, separator), paste0() - without separator
+write.table(Joey_core12, file=paste0(folderpath,"Joey_core12.txt"), row.names = FALSE, quote = FALSE, sep = "\t", na = "")
 
 #=============== 3. FILTER SEARCH RESULTS =================
 # restrict the search by geographical coordinates with an attribute bbox=c(minlon, minlat, maxlon, maxlat)
 # datasets in northern Sweden
 PAGES_Sweden <- pg_search("project:label:PAGES_C-PEAT", count = 1000, bbox=c(17.7, 67.7, 21, 69))
+
 # filter only datasets with "Geochemistry" in title (column citation)
+# grepl function: looks for a given pattern in data
+# filter function from dplyr package: subsetting data (arguments: data frame and logical condition specifying the rows that should be returned)
 PAGES_Sweden <- filter(PAGES_Sweden, grepl("Geochemistry", citation))
 
 #=============== 4. GET MULTIPLE DATA =================
 # combine data into a single data frame
+
+# initiate a data frame
 PAGES_Sweden_data <- data.frame()
 
+# loop over all filtered datastes
+# function bind_rows from dplyr package: unlike for rbind, the number of columns of the dataframes doesn't need to be the same
 for (i in 1:nrow(PAGES_Sweden)) {
   geochem <- pg_data(doi=PAGES_Sweden[i,2])
   geochem <- geochem[[1]]$data
+  # add columns identifying unique source of data for further attribution
   geochem$DOI <- as.character(PAGES_Sweden[i,2])
   geochem$citation <- as.character(PAGES_Sweden[i,5])
   PAGES_Sweden_data <- bind_rows(PAGES_Sweden_data, geochem)
 }
 
-# download table as txt file
-write.table(PAGES_Sweden_data, file=paste(folderpath,"Sweden_geochem.txt", sep=""), row.names = FALSE, quote = FALSE, sep = "\t", na = "")
+# write table as txt file
+write.table(PAGES_Sweden_data, file=paste0(folderpath,"Sweden_geochem.txt"), row.names = FALSE, quote = FALSE, sep = "\t", na = "")
 
 
 #=============== 5. DOWNLOAD DATA FILES VIA PANGAEAR CACHE ===================
@@ -72,13 +81,13 @@ prefix <- "https://download.pangaea.de/dataset/919398/files/"
 # with a condition ("fauna" in Content)
 for (i in (1:nrow(table))){
   if (grepl("fauna", table$'Content'[i]) == TRUE ) {
-    download.file(paste(prefix, table$IMAGE[i], sep=""), destfile = paste(folderpath, table$'IMAGE'[i], sep=""))  
+    download.file(paste0(prefix, table$IMAGE[i]), destfile = paste0(folderpath, table$'IMAGE'[i]))  
   }
 }
 
 #=============== 7. DOWNLOAD MANY FILES ==================
 # this concerns datasets published before 2020 (in .tab file the full path is given, not just the file name)
-# example dataset: https://doi.pangaea.de/10.1594/PANGAEA.910179?format=html#download with several netcdf files
+# example dataset: https://doi.pangaea.de/10.1594/PANGAEA.910179?format=html#download with several netcdf files + a csv
 
 list_of_files <- pg_data(doi="10.1594/PANGAEA.910179")
 list_of_files <- list_of_files[[1]]$data
